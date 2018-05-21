222693616557ee02aaf443769dd18be8afb1748e
tuanVGU <cs2015_tuan.ld@student.vgu.edu.vn>
2018-05-21 22:43:46 +0700

    setup for the first time

---
+++ server.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var path = require("path");
+var express = require('express');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var engine = require('ejs-mate');
+
+var app = express();
+
+// Middleware and config
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(express.static(__dirname +'public'));
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
+//Routing
+
+var main = require('./routes/main');
+app.use('/',main);
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+http.createServer(app).listen(PORT,function(){
+	console.log("App running on port "+ PORT);
+});
